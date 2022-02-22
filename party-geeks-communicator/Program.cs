cb6005bd9b1ef2dde515406a2c8045ef17f3f746
Leonard Atorough <leotorough@gmail.com>
2022-02-22 12:01:39 +0000

    basic project setup

---
+++ party-geeks-communicator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace party_geeks_communicator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
